5adee589ce42c1c14a31e5e94921f68a492f5c12
M Hasnain Khalid <ch.hasnainkhalid@gmail.com>
2019-01-28 00:26:19 +0500

    New Js changes

--- includes/js/admin_js.js
+++ includes/js/admin_js.js
@@ -1,4 +1,49 @@
-jQuery('#posts_pdf').on('change', function() {
-    alert('dsfdsf');
+jQuery(document).ready(function(){
+
+  jQuery('#posts_pdf').on('change', function() {
     //alert( this.value );
+    jQuery('#pdfId').html(this.value);
   });
+
+  jQuery('#zoom_pdf').on('change', function() {
+    //alert( this.value );
+    jQuery('#zoomId').html(this.value);
+  });
+  
+
+  jQuery('#thumbnails_pdf').on('change', function() {
+    //alert( this.value );
+    jQuery('#thumbnailsid').html(this.value);
+  });
+
+  jQuery('#classes_pdf').on('keyup', function() {
+    //alert( this.value );
+    jQuery('#classesid').html(this.value);
+  });
+
+  jQuery('#slide_pdf').on('change', function() {
+    //alert( this.value );
+    jQuery('#slideid').html(this.value);
+  });
+
+
+  jQuery('#flipping_pdf').on('change', function() {
+    //alert( this.value );
+    jQuery('#flippingid').html(this.value);
+  });
+  
+});
+
+function selectText(containerid) {
+  if (document.selection) { // IE
+      var range = document.body.createTextRange();
+      range.moveToElementText(document.getElementById(containerid));
+      range.select();
+  } else if (window.getSelection) {
+      var range = document.createRange();
+      range.selectNode(document.getElementById(containerid));
+      window.getSelection().removeAllRanges();
+      window.getSelection().addRange(range);
+  }
+  console.log('Shortcode is selected');
+}
--- settings.php
+++ settings.php
@@ -6,9 +6,14 @@
 
         <div class="postbox rt-after-title" style="margin-bottom: 0;">
                 <div class="inside">
-                    <p>
+                    <!-- <p>
                         <input type="text" onfocus="this.select();" readonly="readonly" value="[the-post id=&quot;18&quot; title=&quot;Love is life&quot;]" class="large-text code rt-code-sc">
+                    </p> -->
+
+                    <p id="allshortcode" class="shortcode" onclick="selectText('allshortcode')" >
+                        [title="Flip PDF" ThePostid="<span id="pdfId" >XXX</span>" Zoom="<span id="zoomId" >yes</span>" Thumbnails="<span id="thumbnailsid" >yes</span>" Classes="<span id="classesid"></span>" SlideShow="<span id="slideid">yes</span>"   EnableFlippingSound="<span id="flippingid">yes</span>" ]
                     </p>
+
                 </div>
             </div>
 
@@ -19,9 +24,9 @@
                     <label>Select PDF</label>
                 </div>
                 <div class="field">
-                    <select id="posts_pdf" class="inner-filed" >
-                        <option value="1">First</option>
-                        <option value="2" >2nd to check</option>
+                    <select id="posts_pdf" class="inner-filed pdf-check" >
+                        <option value="11111">First</option>
+                        <option value="22222" >2nd to check</option>
                     </select>
                 </div>
             </div>
@@ -33,9 +38,9 @@
                     <label>Show Zoom</label>
                 </div>
                 <div class="field">
-                    <select id="posts_pdf" class="inner-filed" >
-                        <option value="1">Yes</option>
-                        <option value="2" >No</option>
+                    <select id="zoom_pdf" class="inner-filed" >
+                        <option value="yes">Yes</option>
+                        <option value="no" >No</option>
                     </select>
                 </div>
             </div>
@@ -47,9 +52,9 @@
                     <label>Show Thumbnails</label>
                 </div>
                 <div class="field">
-                    <select id="posts_pdf" class="inner-filed" >
-                        <option value="1">Yes</option>
-                        <option value="2" >No</option>
+                    <select id="thumbnails_pdf" class="inner-filed" >
+                        <option value="yes">Yes</option>
+                        <option value="no" >No</option>
                     </select>
                 </div>
             </div>
@@ -61,7 +66,7 @@
                     <label>Extra Classes</label>
                 </div>
                 <div class="field">
-                    <input class="inner-filed" type="text" placeholder="Write classes here please">
+                    <input class="inner-filed" type="text" placeholder="Write space separated classes here" id="classes_pdf">
                 </div>
             </div>
         </div>
@@ -72,9 +77,9 @@
                         <label>Slide Show</label>
                     </div>
                     <div class="field">
-                            <select id="posts_pdf" class="inner-filed" >
-                                    <option value="1">Yes</option>
-                                    <option value="2" >No</option>
+                            <select id="slide_pdf" class="inner-filed" >
+                                    <option value="yes">Yes</option>
+                                    <option value="no" >No</option>
                             </select>
                     </div>
                 </div>
@@ -87,9 +92,9 @@
                         <label>Enable Flipping Sound</label>
                     </div>
                     <div class="field">
-                            <select id="posts_pdf" class="inner-filed" >
-                                    <option value="1">Yes</option>
-                                    <option value="2" >No</option>
+                            <select id="flipping_pdf" class="inner-filed" >
+                                    <option value="yes">Yes</option>
+                                    <option value="no" >No</option>
                             </select>
                     </div>
                 </div>
